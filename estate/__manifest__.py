675a4b39cdbe2789491489936b61d7213989152d
cayleb <lemmyxking17@gmail.com>
2025-12-18 16:00:43 +0300

    Initial estate module structure

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '1.9',
+    'category': 'Real Esate/Estate',
+    'sequence': 15,
+    'summary': 'A real estate advertisement module',
+    'license': 'LGPL-3'
+    'depends': [
+        'base',
+    ],
+}
