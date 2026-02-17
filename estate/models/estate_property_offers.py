b2de10dbadfa1883f859ef126a322bda006d2e52
cayleb <lemmyxking17@gmail.com>
2026-02-17 11:55:41 +0300

    feat:add inverse function to estate property offer

--- estate/models/estate_property_offers.py
+++ estate/models/estate_property_offers.py
@@ -1,10 +1,32 @@
-from odoo import models, fields
+from odoo import models, fields , api
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     _name= "estate.property.offer"
     _description="Estate Property Offer"
     
-    price= fields.Float(string="Offer Price")
+    validity =fields.Integer(default=7)
+    
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        store=True,
+    )
+    @api.depends("validity","create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = base_date + relativedelta(days=record.validity)
+    
+    def _inverse_date_deadline(self):
+        for record in self:
+            base_date = record.create_date.date() if record.create_date else fields.Date.today()
+            
+            if record.date_deadline:
+                record.validity = (record.date_deadline - base_date).days
+                
+    
+    price =fields.Float(string="Offer Price")
     status =fields.Selection(
         [
             ('accepted', 'Accepted'),
