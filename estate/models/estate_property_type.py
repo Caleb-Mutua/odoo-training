35f8c632f1135c6597a9a076402e49cf95dc6acc
cayleb <lemmyxking17@gmail.com>
2026-03-14 11:32:03 +0300

    feat: add sequence field for manual ordering of property types

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,12 +3,13 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name="estate.property.type"
     _description="estate property type"
-    _order = "sequence desc"
+    _order = "sequence, name"
     
     property_ids = fields.One2many('estate.property','property_type_id',string='Properties')
 
     
     name=fields.Char(string='Name' , required=True)
+    sequence = fields.Integer(default=10)
     sequence = fields.Integer(default=10)
     
     
